7e1e489fac6ae85d659837b1430f6d058e876eed
jyroball <jyrojimenez@gmail.com>
2025-08-02 02:40:56 -0700

    fixed endpoints

--- lib/core/config/endpoints.dart
+++ lib/core/config/endpoints.dart
@@ -1,9 +1,5 @@
 class Endpoints {
-  static const baseUrl = "https://jsonplaceholder.typicode.com"; // Example
-
-  // Posts endpoint
+  static const baseUrl = "https://jsonplaceholder.typicode.com";
   static const posts = "/posts";
   static const users = "/users";
-
-  // If you have other endpoints, list them here...
 }
